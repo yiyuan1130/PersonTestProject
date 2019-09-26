264f7c8dfed7c493fc282bc94bd756393db2a61c
李宜源 <it_yiyuan@163.com>
2019-09-26 14:43:50 +0800

    do not know change

---
+++ Assets/CopyContent/CopyContentController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CopyContentController : MonoBehaviour {
+
+	InputField inputBeforeCopy;
+	InputField inputAfterCopy;
+	Button copyBtn;
+
+	void Awake(){
+		inputBeforeCopy = transform.Find ("InputFieldBeforeCopy").GetComponent<InputField>();
+		inputAfterCopy = transform.Find ("InputFieldAfterCopy").GetComponent<InputField>();
+		copyBtn = transform.Find ("CopyButton").GetComponent<Button> ();
+		copyBtn.onClick.AddListener (OnClickCopy);
+	}
+		
+	void OnClickCopy(){
+		GUIUtility.systemCopyBuffer = inputBeforeCopy.text;
+		Debug.LogFormat ("复制的文本：{0}", GUIUtility.systemCopyBuffer);
+	}
+
+}
---
+++ Assets/EventDispatcher/Dispatcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace EventDispatcher{
+	public class GameLogic {
+
+//		delegate renderer;
+
+		void update(){
+			// 计算
+
+			// 渲染
+		}
+	}		
+
+
+	public class ReanderEngine{
+
+		public void render(){
+		
+		}
+
+	}
+}
