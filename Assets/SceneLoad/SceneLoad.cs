8ffe00fb3c677b3959948bbf36537cbe4bca2cf4
liyiyuan <it_yiyuan@163.com>
2020-04-26 15:55:46 +0800

    loadscene

---
+++ Assets/SceneLoad/SceneLoad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour {
+	void Awake(){
+		DontDestroyOnLoad(gameObject);
+		GameObject.Find("Canvas").transform.Find("Button").GetComponent<UnityEngine.UI.Button>().onClick.AddListener(LoadScene);
+	}
+
+	void LoadScene(){
+		SceneManager.LoadScene("Scene2");
+	}
+}
---
+++ Assets/SceneLoad/TestMonoBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMonoBehaviour : MonoBehaviour {
+	void Awake() {
+		Debug.Log("Awake");
+	}
+
+	void OnEnable() {
+		Debug.Log("OnEnable");
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDisable() {
+		Debug.Log("OnDisable");
+	}
+
+	void OnDestroy() {
+		Debug.Log("OnDestroy");
+	}
+
+}
