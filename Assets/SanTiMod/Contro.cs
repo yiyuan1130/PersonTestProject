e69f43ce4e1b6a89428b9577706de4a53cadb038
liyiyuan <it_yiyuan@163.com>
2020-06-17 18:42:18 +0800

    santi

---
+++ Assets/SanTiMod/Contro.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contro : MonoBehaviour {
+
+	public Rigidbody M1;
+	public Rigidbody M2;
+	public Rigidbody m;
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	
+	void Force_M1_M2(){
+
+	}
+
+	void Force_M2_M1(){
+
+	}
+
+	void Force_M1_m(){
+
+	}
+
+	void Force_m_M1(){
+
+	}
+
+	void Force_M2_m(){
+
+	}
+
+	void Force_m_M2(){
+
+	}
+}
