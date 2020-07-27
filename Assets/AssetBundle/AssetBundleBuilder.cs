d566f24410536919e09f00dc7f232e35ecbde39f
liyiyuan@ihuman.com <liyiyuan@ihuman.com>
2020-07-27 18:47:11 +0800

    find objects in editor scene

---
+++ Assets/AssetBundle/AssetBundleBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleBuilder {
+
+	[MenuItem("Tools/AssetBundleBuilder/BuildSelection")]
+	public static void BuildSelection(){
+		// Object[] objs = Selection.GetFiltered(,);
+	}
+
+
+	static void Build(){
+
+	}
+
+}
---
+++ Assets/ForeachObjectsInEditor/Editor/ForeachObjectsInEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class ForeachObjectsInEditor {
+	[MenuItem("Tools/ForeachObjectsInEditor")]
+	public static void ForeachObjects(){
+		string[] sceneArr = {
+			"ForeachObjsScene1",
+			"ForeachObjsScene2",
+			"ForeachObjsScene3",
+		};
+
+		string sceneRootPath = "Assets\\ForeachObjectsInEditor";
+		for (int i = 0; i < sceneArr.Length; i++)
+		{
+			string sceneName = sceneArr[i];
+			string fullPath = Path.Combine(sceneRootPath, sceneName) + ".unity";
+			Scene scene = EditorSceneManager.OpenScene(fullPath, OpenSceneMode.Additive);
+			GameObject[] objects = scene.GetRootGameObjects();
+			Debug.Log("<color=red>======================================</color>");
+			for (int j = 0; j < objects.Length; j++)
+			{
+				Debug.LogFormat("<color=yellow>Scene : {0}, GameObject : {1}</color>", sceneName, objects[j].name);
+			}
+			EditorSceneManager.CloseScene(scene, false);
+		}
+	}
+}
