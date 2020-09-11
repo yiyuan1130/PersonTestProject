0101c2018967f493cc35470a2c5625a09c4c3e66
liyiyuan@ihuman.com <liyiyuan@ihuman.com>
2020-09-11 17:07:57 +0800

    split mesh

--- Assets/SplitMesh/SplitMesh.cs
+++ Assets/SplitMesh/SplitMesh.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class SplitMesh : MonoBehaviour {
 
-	// Use this for initialization
+	public LineRenderer lineRenderer;
+	public GameObject cube;
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+
+	Vector3 panelStartPos;
+	Vector3 panelEndPos;
+	bool drawingPanel = false;
 	void Update () {
+		if (Input.GetMouseButtonDown(0)){
+			panelStartPos = MousPostionToWorld(Input.mousePosition, cube.transform);
+			panelEndPos = panelStartPos;
+			drawingPanel = true;
+		}
+		if (Input.GetMouseButtonUp(0)){
+			drawingPanel = false;
+		}
+		if (drawingPanel) {
+			panelEndPos = MousPostionToWorld(Input.mousePosition, cube.transform);
+			DrawLine();
+		}
+	}
+
+	void DrawLine(){
+		lineRenderer.positionCount = 2;
+		lineRenderer.startWidth = 0.1f;
+		lineRenderer.endWidth = 0.1f;
+		lineRenderer.SetPositions(new Vector3[]{
+			panelStartPos, panelEndPos,
+		});
+	}
+
+	public Vector3 MousPostionToWorld(Vector3 mousePos, Transform targetTransform)
+    {
+        Vector3 dir = targetTransform.position - Camera.main.transform.position;
+        Vector3 normardir = Vector3.Project(dir, Camera.main.transform.forward);
+        return Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, normardir.magnitude));
+    }
+
+	public void CreatePanel(){
+		Vector3 p1 = panelStartPos;
+		Vector3 p2 = panelEndPos;
+		Vector3 dir = p2 - Camera.main.transform.position;
+        Vector3 normardir = Vector3.Project(dir, Camera.main.transform.forward);
+		Vector3 p3 = p2 + normardir;
+
 		
 	}
 }
